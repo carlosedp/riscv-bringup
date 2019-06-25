a703666d8bb94f94ef8d207e569d09a855a25a06
CarlosEDP <me@carlosedp.com>
2019-06-25 17:21:01 -0400

    Accept query params on http. Generate static binaries

--- echo-sample/server.go
+++ echo-sample/server.go
@@ -7,12 +7,20 @@ import (
 	"github.com/labstack/echo"
 )
 
+var name string
+
 func main() {
 	e := echo.New()
         os := runtime.GOOS
         arch := runtime.GOARCH
-        output := fmt.Sprintf("Hello, World! I'm running on %s/%s inside a container!", os, arch)
 	e.GET("/", func(c echo.Context) error {
+		url := c.QueryParams()
+		fmt.Println(url)
+		name = c.QueryParam("name")
+		if name == "" {
+			name = "World"
+		}
+		output := fmt.Sprintf("Hello, %s! I'm running on %s/%s inside a container!", name, os, arch)
 		return c.String(http.StatusOK, output)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
